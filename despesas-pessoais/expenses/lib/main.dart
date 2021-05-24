ef5e92aca6f8d74b1d53e9d7385e56e3491f7850
Myllena Graciani <myllenagraciani@gmail.com>
2021-05-24 14:18:02 -0300

    app da aula da udemy de despesas pessoais

---
+++ despesas-pessoais/expenses/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpenseApp());
+
+class ExpenseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Nova bolsa',
+      value: 210.50,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Internet',
+      value: 119,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Despesas pessoais"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            height: 100,
+            child: Card(
+              color: Colors.amber,
+              child: Text("Gráfico"),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text("Lista de Transações"),
+          )
+        ],
+      ),
+    );
+  }
+}
