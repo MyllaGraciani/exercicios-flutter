0caddc1de0178429c8a45cc2dbff3e89bc5cfccf
Myllena Graciani <myllenagraciani@gmail.com>
2021-05-24 14:28:30 -0300

    ..

--- despesas-pessoais/expenses/lib/main.dart
+++ despesas-pessoais/expenses/lib/main.dart
@@ -46,9 +46,13 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text("Lista de Transações"),
-          )
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
